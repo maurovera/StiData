d0c66df1824801390f346f6ec417a95e9fbd2ebb
catherine <catherine@gmail.com>
2017-03-05 17:34:10 -0300

    validacion
    se captura errores de asignaturas inexistentes

--- StiData-web/src/main/java/servicioRest/AsignaturaServicio.java
+++ StiData-web/src/main/java/servicioRest/AsignaturaServicio.java
@@ -4,6 +4,7 @@ import javax.ejb.EJB;
 import javax.ejb.Stateless;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.NotFoundException;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -25,7 +26,13 @@ public class AsignaturaServicio {
 	@Produces(MediaType.APPLICATION_JSON)
 	@Path("/obtener/{id}")
 	public Asignatura obtenerAsig(@PathParam("id") Integer id) {
-		Asignatura asignatura = asignaturaBean.verAsignatura(id);
+		Asignatura asignatura = new Asignatura();
+		try {
+		asignatura = asignaturaBean.verAsignatura(id);
+		} catch (Exception e) {	
+			   System.out.println(e);		
+	}
 		return asignatura;
 	}
 }
+
