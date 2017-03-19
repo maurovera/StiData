db537267e11311e035c49781920fc91d06659429
catherine <catherine@gmail.com>
2017-03-18 21:04:15 -0300

    Entidades Alumno/estiloAprendizaje
    se crean ambas entidades

---
+++ StiData-ejb/src/main/java/entidades/Alumno.java
@@ -0,0 +1,113 @@
+package entidades;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "alumno")
+public class Alumno implements Serializable{
+	private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue
+    @Column(name = "id_alumno")
+    private Integer idAlumno;
+
+    @OneToOne
+	   private EstiloAprendizaje estilo;
+    
+    @Column(name = "nivel_actual")
+    private String nivelActual;
+    
+    @Column(name = "nombres")
+    private String nombres;
+    
+    @Column(name = "apellidos")
+    private String apellidos;
+    
+    @Size(max = 1)
+    @Column(name = "genero")
+    private String genero;
+    
+    @Column(name = "fecha_nacimiento")
+    private Date fechaNacimiento;
+    
+    @Column(name = "edad")
+    private Integer edad;
+
+	public Integer getIdAlumno() {
+		return idAlumno;
+	}
+
+	public void setIdAlumno(Integer idAlumno) {
+		this.idAlumno = idAlumno;
+	}
+
+	public EstiloAprendizaje getEstilo() {
+		return estilo;
+	}
+
+	public void setEstilo(EstiloAprendizaje estilo) {
+		this.estilo = estilo;
+	}
+
+	public String getNivelActual() {
+		return nivelActual;
+	}
+
+	public void setNivelActual(String nivelActual) {
+		this.nivelActual = nivelActual;
+	}
+
+	public String getNombres() {
+		return nombres;
+	}
+
+	public void setNombres(String nombres) {
+		this.nombres = nombres;
+	}
+
+	public String getApellidos() {
+		return apellidos;
+	}
+
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+
+	public String getGenero() {
+		return genero;
+	}
+
+	public void setGenero(String genero) {
+		this.genero = genero;
+	}
+
+	public Date getFechaNacimiento() {
+		return fechaNacimiento;
+	}
+
+	public void setFechaNacimiento(Date fechaNacimiento) {
+		this.fechaNacimiento = fechaNacimiento;
+	}
+
+	public Integer getEdad() {
+		return edad;
+	}
+
+	public void setEdad(Integer edad) {
+		this.edad = edad;
+	}
+    
+	 @Override
+	    public String toString() {
+	        return "model.Alumno[ idAlumno=" + idAlumno + " ]";
+	    }
+}
---
+++ StiData-ejb/src/main/java/entidades/EstiloAprendizaje.java
@@ -0,0 +1,91 @@
+package entidades;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "estiloAprendizaje")
+public class EstiloAprendizaje implements Serializable{
+	private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue
+    @Column(name = "id_estilo")
+    private Integer idEstilo;
+    
+    @Column(name = "estilo_aprendizaje")
+    private String estiloAprendizaje;
+    
+	//ESTILO VISUAL
+    @Column(name = "resul_v")
+    private Integer resulV;
+    
+	//ESTILO AUDITIVO
+    @Column(name = "resul_a")
+    private Integer resulA;
+    
+  //ESTILO LECTOR
+    @Column(name = "resul_r")
+    private Integer resulR;
+    
+  //ESTILO KINESTESICO
+    @Column(name = "resul_k")
+    private Integer resulK;
+
+	public Integer getIdEstilo() {
+		return idEstilo;
+	}
+
+	public void setIdEstilo(Integer idEstilo) {
+		this.idEstilo = idEstilo;
+	}
+
+	public String getEstiloAprendizaje() {
+		return estiloAprendizaje;
+	}
+
+	public void setEstiloAprendizaje(String estiloAprendizaje) {
+		this.estiloAprendizaje = estiloAprendizaje;
+	}
+
+	public Integer getResulV() {
+		return resulV;
+	}
+
+	public void setResulV(Integer resulV) {
+		this.resulV = resulV;
+	}
+
+	public Integer getResulA() {
+		return resulA;
+	}
+
+	public void setResulA(Integer resulA) {
+		this.resulA = resulA;
+	}
+
+	public Integer getResulR() {
+		return resulR;
+	}
+
+	public void setResulR(Integer resulR) {
+		this.resulR = resulR;
+	}
+
+	public Integer getResulK() {
+		return resulK;
+	}
+
+	public void setResulK(Integer resulK) {
+		this.resulK = resulK;
+	}
+
+	@Override
+    public String toString() {
+        return "model.EstiloAprendizaje[ idEstilo=" + idEstilo + " ]";
+    }
+}
